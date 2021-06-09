bbdbea0d00fadd8eb1000259d5d625a5d6753fc7
Maikol Leiva <maikol.leiva@alumnos.ucn.cl>
2021-06-09 18:11:00 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Login from './Login/Login';
+import Register from './Register/Register';
+function App() {
+  return (
+    <Register/>
+
+  );
+}
+
+export default App;
